022ddf124b8b4b746213e85acad3006fb08fe793
Prince <maihoonprinx@gmail.com>
2024-08-28 19:14:27 +0530

    first commit

---
+++ src/components/Home/Category/Category.jsx
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+
+import "./Category.scss";
+
+const Category = ({ categories }) => {
+
+    const navigate = useNavigate();
+
+    // Check if categories or categories.data is undefined or null
+    if (!categories || !categories.data) {
+        return <div>No categories available</div>;
+    }
+
+    return (
+        <div className="shop-by-category">
+            <div className="categories">
+                {categories?.data?.map((item) => (
+                    <div
+                        key={item.id}
+                        className="category"
+                        onClick={() => navigate(`/category/${item.id}`)}
+                    >
+                        <img
+                            src={
+                                process.env.REACT_APP_DEV_URL +
+                                item.attributes?.img?.data?.attributes?.url || "default-image.jpg"
+                            }
+                            alt={item.attributes?.name || "Category Image"}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Category;
---
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -0,0 +1,76 @@
+import "./SingleProduct.scss";
+import RelatedProducts from "./RelatedProducts/RelatedProducts";
+
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn, FaCartPlus, } from "react-icons/fa";
+import prod from "../../assets/products/earbuds-prod-1.webp";
+
+import useFetch from "../../hooks/useFetch";
+import { useParams } from "react-router-dom";
+
+const SingleProduct = () => {
+    const { id } = useParams();
+    const { data } = useFetch(`/api/products?populate=*&[filters] [id]=${id}`);
+
+    if (!data) return;
+    const product = data.data[0].attributes;
+    return (
+        <div className="single-product-main-content">
+            <div className="layout">
+                <div className="single-product-page">
+                    <div className="left">
+                        <img
+                            src={
+                                process.env.REACT_APP_DEV_URL +
+                                product.img.data[0].attributes.url
+                            }
+                            alt=""
+                        />
+                    </div>
+                    <div className="right">
+                        <span className="name">{product.title}</span>
+                        <span className="price">&#8377;{product.price}</span>
+                        <span className="desc">{product.desc}</span>
+
+                        <div className="cart-buttons">
+                            <div className="quantity-button">
+                                <span>-</span>
+                                <span>5</span>
+                                <span>+</span>
+                            </div>
+                            <button className="add-to-cart-button">
+                                <FaCartPlus />
+                                ADD TO CART
+                            </button>
+                        </div>
+
+                        <span className="divider" />
+
+                        <div className="info-item">
+                            <span className="text-bold">
+                                Category:{" "}
+                                <span>{product.categories.data[0].attributes.title}</span>
+                            </span>
+                            <span className="text-bold">
+                                Share:
+                                <span className="social-icons">
+                                    <FaFacebookF size={16} />
+                                    <FaTwitter size={16} />
+                                    <FaInstagram size={16} />
+                                    <FaLinkedinIn size={16} />
+                                </span>
+                            </span>
+                        </div>
+
+                    </div>
+                </div>
+
+                <RelatedProducts
+                    productId={id}
+                    categoryId={product.categories.data[0].id}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default SingleProduct;
